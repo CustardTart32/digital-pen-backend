6bd2b67f71c0b9c9424f73e624dd848f007a2b25
Cuthbert Zhong <cuthbertzhong333@gmail.com>
2021-07-20 18:45:18 +1000

    Reordered file structure

--- xml_parser/functions/get_ink.py
+++ xml_parser/functions/get_ink.py
@@ -1,6 +1,5 @@
 import json
-
-import numpy as np
+import glob 
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
@@ -8,23 +7,25 @@ from firebase_admin import firestore
 # Use the application default credentials
 # NOTE: This is not a secure way of storing a key. Env variables are preferred.
 # Note that different operating systems may use different private keys 
-cred = credentials.Certificate(
-    "digital-pen-746a6-firebase-adminsdk-18pgv-5541130c80.json")    
+
+json_file = glob.glob("*.json")[0]
+
+cred = credentials.Certificate(json_file)    
 firebase_admin.initialize_app(cred, {
     'projectId': 'digital-pen-746a6',
 })
 
 db = firestore.client()
-
-doc_ref = db.collection("ink").document("87cLwHGOjfeSRggvUXaK")
+doc_id = "KYNjJdlpGzGXUi65FblX"
+doc_ref = db.collection("ink").document(doc_id)
 
 doc = doc_ref.get()
 
-filename = "ink.json"
+filename = "../data/uploaded_ink/" + doc_id + ".json"
 
 if doc.exists:
     ink = doc.to_dict()
-    with open('ink.json', 'w') as fp:
+    with open(filename, 'w') as fp:
         json.dump(ink, fp)
 else:
     print(u'No such document!')

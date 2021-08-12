d7471e8f3a21ab4611f8140e6b7c5d1fa17a4858
Cuthbert Zhong <cuthbertzhong333@gmail.com>
2021-08-13 00:52:56 +1000

    Added new user submissions to visualise

--- xml_parser/functions/get_ink.py
+++ xml_parser/functions/get_ink.py
@@ -1,22 +1,22 @@
 import json
-import glob 
+import glob
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
 # Use the application default credentials
 # NOTE: This is not a secure way of storing a key. Env variables are preferred.
-# Note that different operating systems may use different private keys 
+# Note that different operating systems may use different private keys
 
 json_file = glob.glob("*.json")[0]
 
-cred = credentials.Certificate(json_file)    
+cred = credentials.Certificate(json_file)
 firebase_admin.initialize_app(cred, {
     'projectId': 'digital-pen-746a6',
 })
 
 db = firestore.client()
-doc_id = "856MbTpdBuJtUqfMC1az"
+doc_id = "qYZR7wVjn67RntEL9rpN"
 doc_ref = db.collection("ink").document(doc_id)
 
 doc = doc_ref.get()
